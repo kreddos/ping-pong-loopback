2f929127014a636769e6235418128dc485f917f5
Dmitry Isachenkov <dmitryisachenkov@gmail.com>
2019-06-07 00:04:09 +0400

    исправил баги и добавил монго коннектор

--- common/models/game.js
+++ common/models/game.js
@@ -4,13 +4,17 @@ const createError = require('http-errors');
 module.exports = function(Game) {
   Game.newGame = async (data) => {
     const {firstPlayerId, secondPlayerId} = data.players;
+
     if (!firstPlayerId || !secondPlayerId) {
       throw createError(400, 'fields "firstPlayerId" and "secondPlayerId" are required');
     }
+
     const newGame = await Game.create(data.game);
     await newGame.players.add(firstPlayerId);
     await newGame.players.add(secondPlayerId);
-    return newGame
+    await newGame.save();
+
+    return newGame;
   };
   Game.remoteMethod('newGame', {
     accepts: { arg: 'data', type: 'object', http: { source: 'body' } },
@@ -23,19 +27,19 @@ module.exports = function(Game) {
     if (this.maxSets <= setsCount) {
       throw createError(400, `you can't create set. The game have all sets`)
     }
-    
+
     if (this.status === 'end') {
       throw createError(400, `you can't create set. The game is end`)
     }
 
-    
+
     this.gameSets.create(set);
     const gameSets = await this.gameSets.find();
     if (this.maxSets === gameSets.length) {
       this.status = 'end';
       await this.save();
     }
-    
+
     const game = this.toJSON();
     return { ...game, gameSets };
   }
